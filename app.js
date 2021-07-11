b1f4173bfcee58a2775f75fd09965430e11a0dfb
Brian Jones <brijones.nz@gmail.com>
2021-07-11 15:41:54 +1200

    All complete and functioning as rules specify, even the additional random colour button

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let size = 16;
 const box = document.querySelector('#box');
+const reset = document.querySelector('#reset');
+const resize = document.querySelector('#resize');
+const random = document.querySelector('#rainbow');
 let gridBox;
 
 function setGrid(size) {
+    box.innerHTML = '';
     box.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     box.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < (size * size); i++) {
+    for (let i = 0; i < size * size; i++) {
         let div = document.createElement('div');
         div.classList.add('grid-box');
         box.appendChild(div);
     }
-    gridHover();
-}
-
-function gridHover() {
     gridBox = document.querySelectorAll('.grid-box');
     gridBox.forEach((gb) => {
         gb.addEventListener('mouseover', (e) => {
@@ -21,13 +21,27 @@ function gridHover() {
         });
     });
 }
-function reset() {
+//add listener on button, when clicked it adds a random colour to the moused over background.
+random.addEventListener('click', (e) => {
+    gridBox.forEach((gb) => {
+        gb.addEventListener('mouseover', (e) => {
+            e.target.style.background = `rgb(${randomColour()}, ${randomColour()}, ${randomColour()})`;
+        });
+    });
+})
+//returns the current state to the original look
+reset.addEventListener('click', (e) => {
+    setGrid(size);
+});
+//resizes the grid to the specified new size
+resize.addEventListener('click', (e) => {
     gridBox.forEach((gb) => {
         gb.classList.remove('active');
     });
-}
-function resize() {
-    reset();
-
+    let newSize = prompt("Enter grid size please!");
+    setGrid(newSize);
+});
+function randomColour() {
+    return Math.floor(Math.random() * 255);
 }
 setGrid(size);
